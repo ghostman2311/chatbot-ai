612b579da3dadec5d87f90c86061eb780639dff5
Nikhil <nkarnwal07@gmail.com>
2024-07-02 15:42:02 +0530

    auth layout completed

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SignIn = (props: Props) => {
+  return <div className="flex-1 py-36 md:px-16 w-full">SignIn</div>;
+};
+
+export default SignIn;
---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SignUp = (props: Props) => {
+  return <div className="flex-1 py-36 md:px-16 w-full">signup</div>;
+};
+
+export default SignUp;
